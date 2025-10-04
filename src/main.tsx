81b17570d9b913a3e3f2b32c6c7e123c3a7f8a60
Utkarsh Sharma <utkarshdev2411@gmail.com>
2025-10-04 15:18:36 +0530

    feat: integrate Apollo Client for GraphQL support in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
+import { HttpLink } from '@apollo/client/link/http';
+
+// Created HTTP link
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql'
+});
+
+// Created a new Apollo Client instance
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </BrowserRouter>
   </StrictMode>
-)
+);
